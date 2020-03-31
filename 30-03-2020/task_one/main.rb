2fc4f17158f1b1ad458c881b05338c41d02ed52c
Aleksandr <volkovosnovnaya@gmail.com>
2020-03-31 23:57:26 +0300

    test

---
+++ 30-03-2020/task_one/main.rb
@@ -0,0 +1,12 @@
+require_relative 'complex'
+
+complex_one = MyComplex.new(10, 50)
+complex_two = MyComplex.new(33, -66)
+
+pp complex_one
+puts complex_one
+pp complex_two
+puts complex_two
+
+sum = complex_one.add(complex_two)
+puts "Summ of complex numbers : #{sum}"
